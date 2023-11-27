928df5ee0598939f4b49c01fe7eca6032d22fae7
Hanu <hanu090402@gmail.com>
2023-11-27 21:27:45 +0900

    Returning error message

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class FileUploadController extends Controller
 {
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'pdf' => 'required|file|mimes:pdf|max:2048',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'pdf' => 'required|file|mimes:pdf|max:2048',
+            ]);
 
-        $file = $request->file('pdf');
-        $fileName = time() . '_' . $file->getClientOriginalName();
-        $filePath = $file->storeAs('uploads', $fileName, 'public');
+            $file = $request->file('pdf');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->storeAs('uploads', $fileName, 'public');
 
-        return back()->with('success', 'File has been uploaded.')->with('file', $fileName);
+            return back()->with('success', 'File has been uploaded.')->with('file', $fileName);
+        } catch (\Exception $e) {
+            // Log the error
+            Log::error('File upload failed: ' . $e->getMessage());
+
+            // Return back with an error message
+            return back()->with('error', 'File upload failed.');
+        }
     }
 }
